a89a734591778d26a28602ccdf033494850ea9e4
hankovich <svyatoslav.hankovich@gmail.com>
2016-11-03 18:00:48 +0300

    init commit

---
+++ Task2.Tests/PolinomeTests.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Task2.Tests
+{
+    [TestFixture]
+    public class PolinomeTests
+    {
+        [Test]
+        public void Polinome_P1P2_SumOfP1P2()
+        {
+            Polinome p = new Polinome(1, 2, 3);
+            Polinome p1 = new Polinome(4, 2, 1); 
+            Polinome expected = new Polinome(5, 4, 4);
+            CollectionAssert.AreEqual(expected.Factors, (p + p1).Factors);
+        }
+
+        [Test]
+        public void Polinome_P1P2_OneMoreSumOfP1P2()
+        {
+            Polinome p = new Polinome(1, 2, 3);
+            Polinome p1 = new Polinome(1, 2, 3, 4);
+            Polinome expected = new Polinome(2, 4, 6, 4);
+            CollectionAssert.AreEqual(expected.Factors, (p + p1).Factors);
+        }
+
+        [Test]
+        public void Polinome_P1P2_SubOfP1P2()
+        {
+            Polinome p = new Polinome(1, 2, 3);
+            Polinome p1 = new Polinome(4, 2, 1);
+            Polinome expected = new Polinome(-3, 0, 2);
+            CollectionAssert.AreEqual(expected.Factors, (p - p1).Factors);
+        }
+
+        [Test]
+        public void Polinome_P1P2_Zero()
+        {
+            Polinome p = new Polinome(1, 2, 3);
+            Polinome p1 = new Polinome(1, 2, 3);
+            Polinome expected = new Polinome(0);
+            CollectionAssert.AreEqual(expected.Factors, (p - p1).Factors);
+        }
+
+        [Test]
+        public void Polinome_P1_MinusP1()
+        {
+            Polinome p = new Polinome(1, 2, 3);
+            Polinome expected = new Polinome(-1, -2, -3);
+            CollectionAssert.AreEqual(expected.Factors, (-p).Factors);
+        }
+
+        [Test]
+        public void Polinome_P1_PlusP1()
+        {
+            Polinome p = new Polinome(1, 2, 3);
+            Polinome expected = new Polinome(1, 2, 3);
+            CollectionAssert.AreEqual(expected.Factors, (+p).Factors);
+        }
+
+        [Test]
+        public void Polinome_P1_kP1()
+        {
+            Polinome p = new Polinome(1, 2, 3);
+            Polinome expected = new Polinome(2, 4, 6);
+            CollectionAssert.AreEqual(expected.Factors, (2*p).Factors);
+        }
+
+        [Test]
+        public void Polinome_P1_P10()
+        {
+            Polinome p = new Polinome(1, 2, 3);
+            Polinome expected = new Polinome(0);
+            CollectionAssert.AreEqual(expected.Factors, (p*0).Factors);
+        }
+
+        [Test]
+        public void Polinome_P1P2_MulOfP1P2()
+        {
+            Polinome p = new Polinome(1, 1);
+            Polinome p1 = new Polinome(-1, 1);
+            Polinome expected = new Polinome(-1, 0, 1);
+            CollectionAssert.AreEqual(expected.Factors, (p * p1).Factors);
+        }
+
+        [Test]
+        public void Polinome_P1P2_EqualsOfP1P2()
+        {
+            Polinome p = new Polinome(1, 2, 3);
+            Polinome p1 = new Polinome(1, 2, 3);
+            Assert.AreEqual(true, p1.Equals(p));
+        }
+
+        [Test]
+        public void Polinome_P1P2_NotEqualsOfP1P2()
+        {
+            Polinome p = new Polinome(1, 2, 3);
+            Polinome p1 = new Polinome(4, 2, 1);
+            Assert.AreEqual(false, p1.Equals(p));
+        }
+
+        [Test]
+        public void Polinome_P1P2_GetHashCodeNotEqual()
+        {
+            Polinome p = new Polinome(1, 2, 3);
+            Polinome p1 = new Polinome(4, 2, 1);
+            Assert.AreNotEqual(p.GetHashCode(), p1.GetHashCode());
+        }
+
+        [Test]
+        public void Polinome_P1P2_GetHashCodeEqual()
+        {
+            Polinome p = new Polinome(1, 2, 3, 0);
+            Polinome p1 = new Polinome(1, 2, 3);
+            Assert.AreEqual(p.GetHashCode(), p1.GetHashCode());
+        }
+
+        [Test]
+        public void Polinome_P1_ValueAtPoint()
+        {
+            Polinome p = new Polinome(1, 2, 3, 0, 7);
+            double expected = 1 + 2*2 + 3*2*2 + 7*2*2*2*2;
+            Assert.AreEqual(expected, p.ValueAtPoint(2));
+        }
+    }
+}
---
+++ Task2/Polinome.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+
+    public class Polinome
+    {
+        private readonly double[] factors;
+
+        public Polinome(Polinome old)
+        {
+            factors = new double[old.factors.Length];
+            old.factors.CopyTo(factors, factors.Length);
+        }
+
+        public Polinome(params double[] resource)
+        {
+            if (resource == null)
+                throw new ArgumentNullException();
+            if (resource.Length == 0)
+                throw new ArgumentException();
+
+            int expectedDegree = resource.Length - 1;
+
+            for (; expectedDegree >= 0; expectedDegree--)
+                if (!resource[expectedDegree].Equals(0))
+                    break;
+
+            if (expectedDegree == 0)
+            {
+                factors = new double[expectedDegree + 1];
+                factors[0] = resource[0];
+                return;
+            }
+
+            factors = new double[expectedDegree + 1];
+            for (int i = 0; i <= expectedDegree; i++)
+                factors[i] = resource[i];
+        }
+
+        public int Degree => factors.Length - 1;
+        public double[] Factors => factors;
+
+        public static Polinome operator +(Polinome a, Polinome b)
+        {
+            if (b.Degree > a.Degree)
+            {
+                Swap(ref a, ref b);
+            }
+
+            double[] newFactors = new double[a.Degree + 1];
+
+            a.factors.CopyTo(newFactors, 0);
+
+            for (int i = 0; i <= b.Degree; i++)
+            {
+                newFactors[i] += b.factors[i];
+            }
+
+            return new Polinome(newFactors);
+        }
+
+        public static Polinome operator -(Polinome a, Polinome b)
+        {
+            return a + (-b);
+        }
+
+        public static Polinome operator *(Polinome a, Polinome b)
+        {
+            int degRes = a.Degree + b.Degree;
+            double[] coefficentsRes = new double[degRes + 2];
+            for (int i = 0; i <= a.Degree; i++)
+                for (int j = 0; j <= b.Degree; j++)
+                    coefficentsRes[i + j] += a.factors[i] * b.factors[j];
+            return new Polinome(coefficentsRes);
+        }
+
+        public static Polinome operator -(Polinome a)
+        {
+            return -1*a;
+        }
+
+        public static Polinome operator +(Polinome a)
+        {
+            return +1*a;
+        }
+
+        public static Polinome operator *(double k, Polinome a)
+        {
+            double[] newFactors = new double[a.Degree + 1];
+            for (int i = 0; i <= a.Degree; i++)
+                newFactors[i] = k*a.factors[i];
+            return new Polinome(newFactors);
+        }
+
+        public static Polinome operator *(Polinome a, double k)
+        {
+            return k*a;
+        }
+
+        public static bool operator ==(Polinome a, Polinome b)
+        {
+            if (a.Degree != b.Degree)
+                return false;
+            for (int i = 0; i <= a.Degree; i++)
+            {
+                if (!a.factors[i].Equals(b.factors[i]))
+                    return false;
+            }
+            return true;
+        }
+
+        public static bool operator !=(Polinome a, Polinome b)
+        {
+            return (!(a == b));
+        }
+
+        public double ValueAtPoint(double x)
+        {
+            double result = factors[Degree];
+
+            for (int i = Degree - 1; i >= 0; i--)
+            {
+                result = result*x + factors[i];
+            }
+            return result;
+        }
+
+        public override string ToString()
+        {
+            var result = new StringBuilder(string.Empty);
+            for (int i = Degree; i > 1; i--)
+            {
+                if (!factors[i].Equals(0))
+                    result.Append($"{Sign(factors[i], i)}{Math.Abs(factors[i])}x^{i}");
+            }
+            if (Degree >= 1 && !factors[1].Equals(0))
+                result.Append($"{Sign(factors[1], 1)}{Math.Abs(factors[1])}x");
+            if (!factors[0].Equals(0))
+                result.Append($"{Sign(factors[0], 0)}{Math.Abs(factors[0])}");
+
+            if (result.ToString().Equals(string.Empty))
+                return "0";
+            return result.ToString();
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 0;
+            for (int i = 0; i <= Degree; i++)
+            {
+                hash += RsHash(factors[i].ToString());
+            }
+            return hash;
+        }
+
+        public override bool Equals(object pol)
+        {
+            var polinome = pol as Polinome;
+            return polinome?.factors != null && factors.SequenceEqual(polinome.factors);
+        }
+
+        #region private methods     
+
+        private string Sign(double a, int number)
+        {
+            if (number == this.Degree)
+                return (a > 0) ? string.Empty : "-";
+            return (a > 0) ? "+" : "-";
+        }
+
+
+        private static void Swap(ref Polinome a, ref Polinome b)
+        {
+            Polinome temp;
+            temp = a;
+            a = b;
+            b = temp;
+        }
+
+        private static int RsHash(string str)
+        {
+            const int b = 378551;
+            int a = 63689;
+            int hash = 0;
+
+            foreach (char t in str)
+            {
+                hash = hash*a + t;
+                a *= b;
+            }
+            return hash;
+        }
+
+        #endregion
+    }
+
+}
